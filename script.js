2a761995dea6a87e7f60bad158c0c73e4fbe06d6
loki-trickster <tajb6390@gmail.com>
2022-11-09 12:59:11 +0100

    fixed background image. added some minor javascript to the button

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+const submitBtn = document.querySelector('.submit-button');
+
+submitBtn.addEventListener("click", /*callback*/ onClick);
+
+function onClick () {
+    console.log('click');
+    alert('This is not a real website.');
+}
